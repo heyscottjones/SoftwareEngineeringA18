e2a72f7aa92dfb17ae7daaa3f43d8322697201b6
Scott Jones <smj72@txstate.edu>
2018-04-17 18:47:01 -0500

    updating code

--- src/test/java/flyweighttest/FlyweightTest.java
+++ src/test/java/flyweighttest/FlyweightTest.java
@@ -13,30 +13,40 @@ import org.junit.jupiter.api.Test;
 public class FlyweightTest {
 
   @Test
-  public void testFlyweight() {
+  public void tests1() {
 
     // Create 3 FlyweightSubject instances.
     SharedFlyweightSubject sfS1 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value1");
     SharedFlyweightSubject sfS2 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key2", "value2");
     SharedFlyweightSubject sfS3 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value3");
 
-    // Create 3 UnSharedFlyweight instances. 
-    UnSharedFlyweightSubject usfS1 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value1");
-    UnSharedFlyweightSubject usfS2 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey2","value2");
-    UnSharedFlyweightSubject usfS3 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value3");
-
     // 3 shared flyweight object equivalency tests
     assertNotEquals(sfS1, sfS2);
     assertEquals(sfS1, sfS3);
     assertNotEquals(sfS2, sfS3);
-
+  }
+  
+  @Test
+  public void tests2() {
+    // Create 3 UnSharedFlyweight instances. 
+    UnSharedFlyweightSubject usfS1 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value1");
+    UnSharedFlyweightSubject usfS2 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey2","value2");
+    UnSharedFlyweightSubject usfS3 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value3");
+    
     // 3 unshared flyweight object non-equivalency tests
     assertNotEquals(usfS1, usfS2);
     assertNotEquals(usfS1, usfS3);
     assertNotEquals(usfS2, usfS3);
-
-   // 3 shared flyweight tests assessing the values for the shared (instrinsic) state.
-
+  }
+  
+  @Test
+  public void test3() {
+    // Create 3 FlyweightSubject instances.
+    SharedFlyweightSubject sfS1 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value1");
+    SharedFlyweightSubject sfS2 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key2", "value2");
+    SharedFlyweightSubject sfS3 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value3");
+    
+    // 3 shared flyweight tests assessing the values for the shared (instrinsic) state.
     if (sfS1 instanceof com.hundredwordsgof.flyweightobserver.FlyweightSubject) {
       assertEquals("value1", sfS1.getIntrinsicState());
     }
@@ -48,7 +58,15 @@ public class FlyweightTest {
     if (sfS3 instanceof com.hundredwordsgof.flyweightobserver.FlyweightSubject) {
       assertEquals("value1", sfS3.getIntrinsicState());
     }
-
+  }
+  
+  @Test
+  public void tests4() {
+    // Create 3 UnSharedFlyweight instances. 
+    UnSharedFlyweightSubject usfS1 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value1");
+    UnSharedFlyweightSubject usfS2 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey2","value2");
+    UnSharedFlyweightSubject usfS3 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value3");
+    
     // 3 unshared flyweight tests assessing the values of their states.
 
     if (usfS1 instanceof com.hundredwordsgof.flyweightobserver.UnSharedFlyweightSubject) {
@@ -62,5 +80,30 @@ public class FlyweightTest {
     if (usfS3 instanceof com.hundredwordsgof.flyweightobserver.UnSharedFlyweightSubject) {
       assertEquals("value3", usfS3.getState());
     }
+  }
+  
+  // Testing that, when using setState with an observer attached, both the subject and the object are updated with the int
+  @Test
+  public void newTest1() {
+	  UnSharedFlyweightSubject usfS = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedkey3","value1");
+	  UnSharedFlyweightObserver usfO = new UnSharedFlyweightObserver(usfS);
+	  usfS.attach(usfO);
+	  usfS.setState(3);
+	  
+	  assertEquals(((FlyweightObserver) usfO).getWhatObserverKnows(), usfS.getState());	  
+  }
+  
+  // Testing that, when using setState with the observer dettached AFTER using setState with the 
+  // observer attached to the subject, that the setState method knows to not continue updating the 
+  // observer.
+  @Test
+  public void newTest2() {
+	  UnSharedFlyweightSubject usfS = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedkey3","value4");
+	  UnSharedFlyweightObserver usfO = new UnSharedFlyweightObserver(usfS);
+	  usfS.attach(usfO);
+	  usfS.setState(3);
+	  usfS.dettach(usfO);
+	  usfS.setState(1);
+	  assertNotEquals(((FlyweightObserver) usfO).getWhatObserverKnows(), usfS.getState());
   }
 }
--- src/test/java/observertest/ObserverTest.java
+++ src/test/java/observertest/ObserverTest.java
@@ -22,11 +22,69 @@ public class ObserverTest {
 
     // changes via subject.setState is propagated towards observer
     assertEquals(1, ((FlyweightObserver) observer).getWhatObserverKnows());
+  }
+  
+  @Test
+  public void testDettach() throws CloneNotSupportedException {
+    FlyweightSubjectFactory fwSFactory = new FlyweightSubjectFactory();
+
+    FlyweightSubject subject = (FlyweightSubject) fwSFactory.getSubject("ObserverTest", "testSubject");
+
+    UnSharedFlyweightObserver observer = new UnSharedFlyweightObserver(subject);
+    subject.attach(observer);
+    subject.setState(1);
+
+    // changes via subject.setState is propagated towards observer
+    assertEquals(1, ((FlyweightObserver) observer).getWhatObserverKnows());
 
     subject.dettach(observer);
     subject.setState(0);
 
     // observer is detached so changes are not propageted
     assertEquals(1, ((FlyweightObserver) observer).getWhatObserverKnows());
+  }
+  
+  // Testing that multiple observers are updated when the setState method (which contains notifyObservers)
+  @Test
+  public void newTests1() {
+	FlyweightSubjectFactory fwSFactory = new FlyweightSubjectFactory();
+	
+	FlyweightSubject subject = (FlyweightSubject) fwSFactory.getSubject("ObserverTest", "testSubject");
+	
+	UnSharedFlyweightObserver observer1 = new UnSharedFlyweightObserver(subject);
+	UnSharedFlyweightObserver observer2 = new UnSharedFlyweightObserver(subject);
+	UnSharedFlyweightObserver observer3 = new UnSharedFlyweightObserver(subject);
+	subject.attach(observer1);
+	subject.setState(1);
+	subject.attach(observer2);
+	subject.setState(3);
+	subject.dettach(observer2);
+	subject.attach(observer3);
+	subject.setState(2);
+	
+	assertEquals(((FlyweightObserver) observer1).getWhatObserverKnows(), ((FlyweightObserver) observer3).getWhatObserverKnows());
+	assertNotEquals(((FlyweightObserver) observer1).getWhatObserverKnows(), ((FlyweightObserver) observer2).getWhatObserverKnows());
+	assertNotEquals(((FlyweightObserver) observer3).getWhatObserverKnows(), ((FlyweightObserver) observer2).getWhatObserverKnows());
+  }
+  
+  // Testing if the observers are notified if we call notifyObservers after we have set the state
+  @Test
+  public void newTests2() {
+	FlyweightSubjectFactory fwSFactory = new FlyweightSubjectFactory();
+	
+	FlyweightSubject subject = (FlyweightSubject) fwSFactory.getSubject("ObserverTest", "testSubject");
+	
+	UnSharedFlyweightObserver observer1 = new UnSharedFlyweightObserver(subject);
+	UnSharedFlyweightObserver observer2 = new UnSharedFlyweightObserver(subject);
+	UnSharedFlyweightObserver observer3 = new UnSharedFlyweightObserver(subject);
+	subject.attach(observer1);
+	subject.setState(1);
+	subject.attach(observer2);
+	subject.attach(observer3);
+	subject.notifyObservers();
+	
+	assertEquals(((FlyweightObserver) observer1).getWhatObserverKnows(), ((FlyweightObserver) observer3).getWhatObserverKnows());
+	assertEquals(((FlyweightObserver) observer1).getWhatObserverKnows(), ((FlyweightObserver) observer2).getWhatObserverKnows());
+	assertEquals(((FlyweightObserver) observer3).getWhatObserverKnows(), ((FlyweightObserver) observer2).getWhatObserverKnows());
   }
 }
